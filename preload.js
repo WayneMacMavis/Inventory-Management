10e0eccf63630c63036e1f4e11919e464f2a4620
Wayne Mac Mavis <w.macmavis83@gmail.com>
2025-02-16 20:52:06 +0200

    Production Build

--- preload.js
+++
@@ -1,6 +0,0 @@
-// preload.js
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electron', {
-  initiateDownload: (downloadUrl) => ipcRenderer.send('initiate-download', downloadUrl),
-});
